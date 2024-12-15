2e9df4ecade4d8be9599b1e1a511ac1787a1fa7d
karolleq268 <karolleq268@proton.me>
2024-12-15 21:08:43 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,7 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    std::for_each(lista.begin(), lista.end(), [&retval](Human& e) {
+    std::for_each(people.begin(), people.end(), [&retval](Human& e) {
         e.birthday();
         retval.insert(retval.begin(), e.isMonster() ? 'n' : 'y');
     });
